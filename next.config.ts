40b49bf5219588ff178bba6a370623f7d71b0a00
yasaswib-2004 <tools@luxhospitals.com>
2026-05-13 18:49:39 +0530

    career and calendar pages created

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/calendar",
+        destination: "/about/calendar",
+        permanent: false,
+      },
+      {
+        source: "/career",
+        destination: "/careers",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
