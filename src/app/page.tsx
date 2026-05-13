0c9aa1b506b7d1e19e464b319e400d7a44f2566f
Ganesh <205735160+luxHospitlas@users.noreply.github.com>
2026-05-13 12:58:55 +0530

    blank site

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center font-sans">
+      <h1 className="text-2xl font-medium text-gray-900">Welcome</h1>
+    </main>
+  );
+}
